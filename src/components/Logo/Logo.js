facd7eb8cccbf92be5f98ef406706c49bb7fcef5
iankii <ankit.pal2011@gmail.com>
2019-01-06 19:32:46 +0530

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Logo.css';
+
+import burgerLogo from './../../assets/images/burger-logo.png';
+
+const logo = () => {
+  return (
+      <div className={classes.Logo}>
+          <img src={burgerLogo} alt="MyBurger" />
+      </div>
+  );
+}
+
+export default logo;
